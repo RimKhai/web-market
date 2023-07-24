abeed7e42b31766f0469d6d876cb4f3ef91a9ef7
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2023-07-24 11:08:12 +0300

    Add Link to catalog from navigation

--- src/widgets/NavigationBar.tsx
+++ src/widgets/NavigationBar.tsx
@@ -2,11 +2,14 @@ import React from "react"
 import UIIcon from "../ui_kit/ui_icon/UIIcon"
 import ProfileNavButton from "../entites/profile_nav_button/ProfileNavButton"
 import ShoppingCartButton from "../entites/shopping_cart_button/ShoppingCartButton"
+import { NavLink } from "react-router-dom"
 
 const NavigationBar: React.FunctionComponent = () => {
     return (
         <div className='w-full h-16 bg-red-500 flex items-center justify-between px-8'>
-            <div className='text-white text-2xl h-8'>КАТАЛОГ</div>
+            <NavLink to={'/'}>
+                <div className='text-white text-2xl h-8'>КАТАЛОГ</div>
+            </NavLink>
             <div className='flex space-x-8'>
                 <ProfileNavButton />
                 <ShoppingCartButton />
