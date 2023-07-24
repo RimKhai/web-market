01a36dde65ae38f5e6f14b01fb2e8312a41b7faa
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2023-07-24 10:53:56 +0300

    Add Link Button to Catalog for Auth, Reg

--- src/widgets/Authorization.tsx
+++ src/widgets/Authorization.tsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom"
 
 const Authorization: React.FunctionComponent = () => {
     return (
-        <div className='flex justify-center items-center'>
+        <div className='flex flex-col justify-center items-center'>
             <UIBlock styles='w-[33%] mt-16'>
                 <div className='flex place-self-center text-3xl mb-4'>
                     Вход в личный аккаунт
@@ -16,6 +16,7 @@ const Authorization: React.FunctionComponent = () => {
                 <UIButton>Войти</UIButton>
                 <NavLink className='flex place-self-end text-blue-500 underline'  to={"/reg"}>Нет Аккаунт? Создать!</NavLink>
             </UIBlock>
+            <NavLink className='p-4 w-[33%] text-center bg-white rounded-[20px] hover:text-white hover:bg-red-600 duration-1000 mt-16'  to={"/"}>Перейти в каталог</NavLink>
         </div>
     )
 }
--- src/widgets/Registration.tsx
+++ src/widgets/Registration.tsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom"
 
 const Registration: React.FunctionComponent = () => {
     return (
-        <div className='flex justify-center items-center'>
+        <div className='flex flex-col justify-center items-center'>
             <UIBlock styles='w-[33%] mt-16'>
                 <div className='flex place-self-center text-3xl mb-4'>
                     Регистрация
@@ -20,6 +20,7 @@ const Registration: React.FunctionComponent = () => {
                 <UIButton>Зарегистрироваться</UIButton>
                 <NavLink className='flex place-self-end text-blue-500 underline'  to={"/auth"}>Уже есть аккаунт</NavLink>
             </UIBlock>
+            <NavLink className='p-4 w-[33%] text-center bg-white rounded-[20px] hover:text-white hover:bg-red-600 duration-1000 mt-16'  to={"/"}>Перейти в каталог</NavLink>
         </div>
     )
 }
