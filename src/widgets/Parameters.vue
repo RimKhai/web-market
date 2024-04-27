0c863bc4dae0e30724e978b28521d5f1a40c05f8
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-04-27 21:42:47 +0300

    Product Parameters

---
+++ src/widgets/Parameters.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import Typography from '../shared/Typography.vue'
+
+    const $props = defineProps(['parameters'])
+</script>
+
+<template>
+    <div class="param__container">
+        <div
+            v-for="key in Object.keys(parameters)"
+            class="param"
+            :key="key">
+            <Typography>{{ key }}</Typography>
+            <Typography>{{ parameters[key] }}</Typography>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .param__container {
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+    }
+    .param {
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem;
+        background-color: rgba($color: $primary-color, $alpha: 0.05);
+        transition-duration: 500ms;
+        border-radius: $border-large;
+        &:hover {
+            background-color: rgba($color: $accent-color, $alpha: 0.4);
+        }
+    }
+</style>
