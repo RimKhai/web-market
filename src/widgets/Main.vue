0df21530d23c8a603e63ac004f20b9b21501112c
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-04-27 21:29:19 +0300

    Main Component

---
+++ src/widgets/Main.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import Header from './Header.vue'
+    import { RouterView } from 'vue-router'
+</script>
+
+<template>
+    <Header class="header"></Header>
+    <main class="main">
+        <RouterView />
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    .main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: 40px;
+        margin-top: 80px;
+    }
+</style>
