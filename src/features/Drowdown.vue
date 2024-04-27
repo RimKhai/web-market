f247250103378688b5269f6a0cf365e54187a055
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-04-27 21:47:59 +0300

    Dropdown component

---
+++ src/features/Drowdown.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const isOpen = ref(false)
+
+    const openDisclosure = () => {
+        isOpen.value = true
+    }
+
+    const closeDisclosure = () => {
+        setTimeout(() => (isOpen.value = false), 5000)
+    }
+</script>
+
+<template>
+    <div
+        :class="['dropdown', `is_open_${isOpen}`]"
+        @mouseleave="closeDisclosure">
+        <div
+            class="dropdown__button"
+            @mouseenter="openDisclosure">
+            <slot name="button"></slot>
+        </div>
+        <div
+            class="dropdown__panel"
+            v-show="isOpen">
+            <slot name="panel"></slot>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .dropdown_button {
+        padding: 24px;
+    }
+</style>
