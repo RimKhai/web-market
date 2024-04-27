4c031ced3c3e0fe874d59b642f9d6ed509a9108c
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-04-27 21:38:31 +0300

    Icons component

---
+++ src/shared/BaseIcon.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const props = defineProps(['type', 'fill'])
+    import icons from '../icons.js'
+</script>
+
+<template>
+    <svg
+        :class="$attrs.class || ' w-6 h-6'"
+        :fill="fill || 'currentColor'"
+        stroke="currentColor"
+        view-box="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+        v-html="icons[type]"
+        data-testid="icon"></svg>
+</template>
+
+<style lang="scss" scoped></style>
