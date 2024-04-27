631a589801939fd92bf8510c7da2595347d740ed
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-04-27 21:26:36 +0300

    Initialize

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import './index.scss'
+import App from './App.vue'
+
+import { router } from './router.js'
+
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
+
+
+
+createApp(App).use(pinia).use(router).mount('#app')
