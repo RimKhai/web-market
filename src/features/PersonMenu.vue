6a154aa300809936acb09b42a9e5412a67d88a1c
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-05-06 18:16:53 +0300

    Person Menu

---
+++ src/features/PersonMenu.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import Typography from '../shared/Typography.vue'
+    import BaseIcon from '../shared/BaseIcon.vue';
+
+    const props = defineProps({
+        name: {
+            type: String,
+            required: true,
+        },
+        secondName: {
+            type: String,
+            required: true,
+        },
+    })
+</script>
+
+<template>
+    <RouterLink class="header__person-menu" to="/profile">
+            <BaseIcon type="profile" />
+            <Typography tag_name="p">
+                {{ `${name} ${secondName}` }}
+            </Typography>
+    </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+.header__person-menu {
+    display: flex;
+    align-items: center;
+    gap: 0 4px;
+}
+</style>
