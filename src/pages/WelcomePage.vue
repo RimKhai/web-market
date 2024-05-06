069e5f2f8dd4e5ec6a61621e77e662cf2eab2c2b
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-05-06 18:21:15 +0300

    Welcom Page(optional)

---
+++ src/pages/WelcomePage.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import { usePersonStore } from '../stores/PersonStore'
+    import Typography from '../shared/Typography.vue';
+    import { router } from '../router'
+
+    const $person_store = usePersonStore()
+
+    setTimeout(() => router.push('/'), 3000)
+</script>
+
+<template>
+    <Typography tag_name="h1">Добро пожаловать, {{ $person_store.getCurrentPerson.name }}</Typography>
+    <Typography>Перенаправление на страницу категорий через 3 секунд...</Typography>
+</template>
+
+<style lang="scss" scoped></style>
