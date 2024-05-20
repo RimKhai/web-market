59bd62174e10cf5abf29f5f26ae4dbd14252eee9
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-05-20 20:56:06 +0300

    fix: Pagination numbers

--- src/features/Pagination.vue
+++ src/features/Pagination.vue
@@ -1,30 +1,30 @@
 <script setup>
-    import { computed } from 'vue'
-    import Button from '../shared/Button.vue'
+import { computed } from 'vue'
+import Button from '../shared/Button.vue'
 
-    const $emits = defineEmits(['changePage'])
+const $emits = defineEmits(['changePage'])
 
-    const $props = defineProps({
-        items_count: {
-            type: Number,
-            required: true,
-        },
-        items_per_page: {
-            type: Number,
-            required: true,
-        },
-        current_page: {
-            type: Number,
-            required: true,
-        },
-    })
+const $props = defineProps({
+    items_count: {
+        type: Number,
+        required: true,
+    },
+    items_per_page: {
+        type: Number,
+        required: true,
+    },
+    current_page: {
+        type: Number,
+        required: true,
+    },
+})
 
-    const _page_count = computed(() =>
-        Math.ceil($props.items_count / $props.items_per_page)
-    )
-    const _pages = computed(() =>
-        Array.from({ length: _page_count.value }, (_, i) => i + 1)
-    )
+console.log($props.items_count)
+console.log($props.items_per_page)
+console.log($props.current_page)
+
+const _page_count = computed(() => Math.ceil($props.items_count / $props.items_per_page))
+const _pages = computed(() => Array.from({ length: _page_count.value }, (_, i) => i + 1))
 </script>
 
 <template>
@@ -32,31 +32,33 @@
         <slot></slot>
         <div
             v-show="items_count > items_per_page"
-            class="flex space-x-1">
+            class="flex space-x-1"
+        >
             <Button
                 v-if="current_page != 1 && current_page > 2"
-                @click="$emit('changePage', 1)">
-                {{ 1 }}
+                @click="$emit('changePage', 1)"
+            >
+                1
             </Button>
             <Button
                 v-if="current_page > 1"
-                @click="$emit('changePage', current_page - 1)">
-                {{ currentPage - 1 }}
+                @click="$emit('changePage', current_page - 1)"
+            >
+                {{ current_page - 1 }}
             </Button>
             <Button class="is_active_true">
-                {{ currentPage }}
+                {{ current_page }}
             </Button>
             <Button
                 v-if="current_page + 1 <= _pages.length"
-                @click="$emit('changePage', current_page + 1)">
+                @click="$emit('changePage', current_page + 1)"
+            >
                 {{ current_page + 1 }}
             </Button>
             <Button
-                v-if="
-                    current_page != _pages.length &&
-                    _pages.length != current_page + 1
-                "
-                @click="$emit('changePage', _pages.length)">
+                v-if="current_page != _pages.length && _pages.length != current_page + 1"
+                @click="$emit('changePage', _pages.length)"
+            >
                 {{ _pages.length }}
             </Button>
         </div>
@@ -64,7 +66,7 @@
 </template>
 
 <style lang="scss" scoped>
-    .is_active_true {
-        background-color: $accent-color;
-    }
+.is_active_true {
+    background-color: $accent-color;
+}
 </style>
